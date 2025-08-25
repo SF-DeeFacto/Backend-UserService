8930427f7b991d13b6d0c7dfe2fc0906c5208bb5
goozip2 <92193151+goozip2@users.noreply.github.com>
2025-08-26 08:47:32 +0900

    feat | sprint4 | LC-000 | @RestControllerAdvice 기반 전역 예외 처리 추가 | SHUN

--- user-service/src/main/java/com/deefacto/user_service/advice/ApiCommonAdvice.java
+++ user-service/src/main/java/com/deefacto/user_service/advice/ApiCommonAdvice.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ApiCommonAdvice {
     @ExceptionHandler(CustomException.class)
-
     public ResponseEntity<ApiResponseDto<String>> handleCustomException2(CustomException e) {
         ErrorCodeInterface errorCode = e.getErrorCode();
         return ResponseEntity
@@ -23,6 +22,14 @@ public class ApiCommonAdvice {
                         errorCode.getCode(),       // "COMMON404" 같은 에러코드
                         e.getMessage()     // 메시지
                 ));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponseDto<String>> handleException(Exception e) {
+        log.error("Unhandled exception: ", e);
+        return ResponseEntity
+                .status(500)
+                .body(ApiResponseDto.createError("COMMON500", "Internal Server Error"));
     }
 }
 
